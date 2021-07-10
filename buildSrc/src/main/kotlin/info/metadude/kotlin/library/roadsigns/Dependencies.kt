6ab36f087eb545896ed881fa8b6f739b8ef2c323
Tobias Preuss <tobias.preuss@googlemail.com>
2021-07-10 10:43:12 +0200

    Use appcompat v.1.3.1.
    + Changelog: https://developer.android.com/jetpack/androidx/releases/appcompat#version_131_3.

--- buildSrc/src/main/kotlin/info/metadude/kotlin/library/roadsigns/Dependencies.kt
+++ buildSrc/src/main/kotlin/info/metadude/kotlin/library/roadsigns/Dependencies.kt
@@ -27,7 +27,7 @@ object Libs {
 
     private object Versions {
         const val annotation = "1.2.0"
-        const val appCompat = "1.2.0"
+        const val appCompat = "1.3.1"
         const val constraintLayout = "2.0.4"
         const val espresso = "3.3.0"
         const val testExtJunit = "1.1.2"
