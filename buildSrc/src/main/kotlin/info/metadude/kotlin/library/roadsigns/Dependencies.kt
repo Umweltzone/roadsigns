fb3adabd8beeb4d2e3601c0ae77aae7029a322b2
Tobias Preuss <tobias.preuss@googlemail.com>
2023-03-06 17:53:18 +0100

    Use appcompat v.1.6.1.
    + Changelog: https://developer.android.com/jetpack/androidx/releases/appcompat#version_161_3.

--- buildSrc/src/main/kotlin/info/metadude/kotlin/library/roadsigns/Dependencies.kt
+++ buildSrc/src/main/kotlin/info/metadude/kotlin/library/roadsigns/Dependencies.kt
@@ -28,7 +28,7 @@ object Libs {
 
     private object Versions {
         const val annotation = "1.6.0"
-        const val appCompat = "1.4.2" // compileSdk 32 is required as of 1.5.0
+        const val appCompat = "1.6.1"
         const val constraintLayout = "2.1.4"
         const val espresso = "3.5.1"
         const val testExtJunit = "1.1.5"
